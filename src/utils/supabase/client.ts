b3d00c15d2a82a4af7cec6c37d1ae028ffac103a
seokgyeong Lee <ho04029@naver.com>
2026-08-05 16:37:43 +0900

    feat: supabase 데이터타입

--- src/utils/supabase/client.ts
+++ src/utils/supabase/client.ts
@@ -1,7 +1,8 @@
 import { createBrowserClient } from '@supabase/ssr';
+import { Database } from './database.type';
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
 const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
 
 // Supabase 클라이언트 인스턴스 생성
-export const supabase = createBrowserClient(supabaseUrl, supabaseKey);
+export const supabase = createBrowserClient<Database>(supabaseUrl, supabaseKey);
