0af9477f45186ce88afbf1c6b99a855c6e1c96c3
seokgyeong Lee <ho04029@naver.com>
2026-08-10 17:31:02 +0900

    feat: 좋아요 페이지 여행기 쓰기 버튼 추가

--- src/app/(main)/favorite/page.tsx
+++ src/app/(main)/favorite/page.tsx
@@ -1,9 +1,11 @@
 'use client';
 
 import { useEffect, useState } from 'react';
-import { Heart, MapPin } from 'lucide-react';
+import { useRouter } from 'next/navigation';
+import { Heart, MapPin, FileText } from 'lucide-react';
 import { supabase } from '@/utils/supabase/client';
 import { Card, CardContent } from '@/components/Card';
+import { Button } from '@/components/Button';
 import { QueryData } from '@supabase/supabase-js';
 
 const query = supabase.from('favorite_destinations').select(`
@@ -20,6 +22,7 @@ const query = supabase.from('favorite_destinations').select(`
 type Favorites = QueryData<typeof query>;
 
 export default function FavoritePage() {
+  const router = useRouter();
   const [favorites, setFavorites] = useState<Favorites>([]);
   const [loading, setLoading] = useState(true);
 
@@ -61,20 +64,41 @@ export default function FavoritePage() {
   }
 
   return (
-    <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-3">
+    <div className="grid gap-4 px-4 py-4 md:grid-cols-2 lg:grid-cols-3">
       {favorites.map((favorite) => (
         <Card key={favorite.id}>
           <CardContent className="flex items-center justify-between p-6">
             <div>
-              <h3 className="font-semibold">{favorite.destinations.name}</h3>
+              <h3 className="mb-2 font-semibold">
+                {favorite.destinations.name}
+              </h3>
 
-              <div className="text-muted-foreground mt-2 flex items-center gap-1 text-sm">
-                <MapPin className="h-4 w-4" />
-                {favorite.destinations.province}
+              <div className="mb-3 flex flex-wrap gap-2">
+                <span className="inline-flex items-center rounded bg-blue-100 px-2 py-1 text-xs text-blue-700">
+                  <MapPin className="mr-1 h-3 w-3" />
+                  {favorite.destinations.province
+                    ? favorite.destinations.province
+                    : favorite.destinations.name}
+                </span>
               </div>
             </div>
-
-            <Heart className="h-5 w-5 fill-red-500 text-red-500" />
+            <div className="flex flex-col items-center gap-1">
+              <Heart className="h-5 w-5 fill-red-500 text-red-500" />
+              <Button
+                variant="outline"
+                size="sm"
+                className="mt-4"
+                onClick={() =>
+                  // todo: 경로 어떻게 할지 좀 더 생각해보기
+                  router.push(
+                    `/trips/new?destination=${favorite.destinations.id}`,
+                  )
+                }
+              >
+                <FileText />
+                여행기 쓰기
+              </Button>
+            </div>
           </CardContent>
         </Card>
       ))}
